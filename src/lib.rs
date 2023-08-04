d749a2a761cdcf3bf402b83b3f6a1ceec6f6b638
ian171 <5502115+ian171@user.noreply.gitee.com>
2023-08-04 20:49:23 +0800

    code(email): add sign fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! 阿里云各种云服务功能的sdk
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod email;
