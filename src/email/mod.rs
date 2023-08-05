d172ddb66e82a499a9eb6d6588d4c58754524255
ian171 <5502115+ian171@user.noreply.gitee.com>
2023-08-05 16:03:07 +0800

    refactor(utils): add utils

--- src/email/mod.rs
+++ src/email/mod.rs
@@ -1,51 +1,6 @@
 //! ali cloud email sdk
 
-use base64::{engine::general_purpose, Engine};
-use hmac::{Hmac, Mac};
-use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
-use sha1::Sha1;
-use std::collections::BTreeMap;
-use url::form_urlencoded;
+pub(crate) mod utils;
 
-mod pub_params;
-mod send_email;
-
-// 定义一个函数，用于计算签名字符串
-fn sign_params(query_params: &BTreeMap<String, String>, access_key_secret: &str) -> String {
-    let canonicalized_query_string = form_urlencoded::Serializer::new(String::new())
-        .extend_pairs(query_params)
-        .finish()
-        .replace("+", "%20")
-        .replace("*", "%2A")
-        .replace("%7E", "~");
-
-    let percent_encode_string = utf8_percent_encode(&canonicalized_query_string, NON_ALPHANUMERIC)
-        .to_string()
-        .replace("%2D", "-")
-        .replace("%5F", "_")
-        .replace("%2E", ".");
-
-    let string_to_sign = format!("{}&{}&{}", "POST", "%2F", percent_encode_string);
-
-    type HmacSha1 = Hmac<Sha1>;
-    let mut mac =
-        HmacSha1::new_from_slice(format!("{}&", access_key_secret).as_bytes()).expect("error key");
-    mac.update(string_to_sign.as_bytes());
-    let res = mac.finalize().into_bytes();
-
-    general_purpose::STANDARD.encode(res)
-}
-
-#[test]
-fn sign_params_test() {
-    let s = "AccessKeyId=testid&AccountName=<a%b'>&Action=SingleSendMail&AddressType=1&Format=XML&HtmlBody=4&RegionId=cn-hangzhou&ReplyToAddress=true&SignatureMethod=HMAC-SHA1&SignatureNonce=c1b2c332-4cfb-4a0f-b8cc-ebe622aa0a5c&SignatureVersion=1.0&Subject=3&TagName=2&Timestamp=2016-10-20T06:27:56Z&ToAddress=1@test.com&Version=2015-11-23";
-    let url = url::Url::parse(&format!("http://example.com?{}", s)).unwrap();
-    let mut map = BTreeMap::new();
-    for (key, value) in url.query_pairs() {
-        map.insert(key.to_string(), value.to_string());
-    }
-
-    let sign = sign_params(&map, "testsecret");
-
-    assert_eq!(sign, "llJfXJjBW3OacrVgxxsITgYaYm0=")
-}
+pub mod pub_params;
+pub mod send_email;
