b1ec4debc544b25233b53aa7dbda0944f5dd3bcd
ian171 <5502115+ian171@user.noreply.gitee.com>
2024-07-21 19:46:28 +0800

    refactor(email): remove blocking request

--- tests/email/main.rs
+++ tests/email/main.rs
@@ -1,6 +1,5 @@
 mod test_config;
 
-use u_ali_sdk::blocking::email as email_blocking;
 use u_ali_sdk::email;
 use u_ali_sdk::error::Error;
 
@@ -9,28 +8,6 @@ use u_ali_sdk::error::Error;
 fn get_test_conf() {
     let s = test_config::AliConfig::get_conf();
     println!("{:?}", s);
-}
-
-#[test]
-fn blocking_single_send_email() {
-    let conf = test_config::AliConfig::get_conf();
-    let client = email_blocking::EmailSdk::new(conf.access_key_id, conf.access_key_secret, None);
-    let params = email::send_email::SingleSendEmailParams {
-        account_name: &conf.account_name,
-        address_type: "1",
-        reply_to_address: "false",
-        subject: "test_subject邮件发送测试",
-        to_address: &conf.to_address,
-        click_trace: None,
-        from_alias: None,
-        html_body: Some("这是一个测试（blocking）"),
-        tag_name: None,
-        text_body: None,
-        reply_address: None,
-        reply_address_alias: None,
-    };
-
-    client.single_send_email(&params);
 }
 
 #[tokio::test]
