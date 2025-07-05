d02347974a3493ff47bca0590389baf74f9c3f87
ian373 <58252781+ian373@users.noreply.github.com>
2025-07-05 18:13:47 +0800

    refactor(*): Update message sending logic use builder mode

--- src/server_chan/mod.rs
+++ src/server_chan/mod.rs
@@ -1,121 +1,95 @@
-use crate::error::Error;
+use bon::{Builder, bon};
+use reqwest::StatusCode;
 use serde::Serialize;
 
-#[derive(Default)]
-pub struct TurboSendParams<'a> {
-    pub title: &'a str,
-    /// 消息内容
-    pub description: Option<&'a str>,
-    pub short: Option<&'a str>,
-    pub is_no_ip: bool,
-    /// 最多支持两个通道，如果输入的通道超过两个，只取前两个
-    pub channel: Option<&'a [u8]>,
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("request failed: code: {code}\nbody: {body}")]
+    API { code: StatusCode, body: String },
+    #[error("use reqwest error:\n {0}")]
+    Reqwest(#[from] reqwest::Error),
 }
 
-/// 使用Server酱Turbo
-pub struct ServerChanTurbo<'a> {
-    token: &'a str,
-    client: reqwest::Client,
-}
-
-impl<'a, 'b> ServerChanTurbo<'a>
-where
-    'a: 'b,
-{
-    pub fn new(token: &'a str) -> Self {
-        Self {
-            token,
-            client: reqwest::Client::new(),
-        }
-    }
-
-    pub async fn send_msg_turbo(&self, params: TurboSendParams<'b>) -> Result<(), Error> {
-        if params.title.is_empty() {
-            return Err(Error::AnyError("title can't be empty.".to_string()));
-        }
-
-        let mut p = Vec::with_capacity(5);
-        p.push(("title", params.title));
-        if let Some(description) = params.description {
-            p.push(("desp", description));
-        }
-        if let Some(short) = params.short {
-            p.push(("short", short));
-        }
-        if params.is_no_ip {
-            p.push(("noip", "1"));
-        }
-
-        let channel_str;
-        if let Some(channel) = params.channel {
-            let mut iter = channel.iter();
-            channel_str = match (iter.next(), iter.next()) {
-                (Some(c1), None) => c1.to_string(),
-                (Some(c1), Some(c2)) => format!("{}|{}", c1, c2),
-                _ => {
-                    return Err(Error::AnyError(
-                        "The number of channels can only be 1 or 2".to_string(),
-                    ))
-                }
-            };
-            p.push(("channel", &channel_str));
-        }
-
-        let url = format!("https://sctapi.ftqq.com/{}.send", self.token);
-        self.client.post(url).form(&p).send().await?;
-
-        Ok(())
-    }
-}
-
-#[derive(Serialize)]
-pub struct SendParams<'a> {
-    /// 推送的标题
-    pub title: &'a str,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    /// 推送的正文内容，则为必填，支持markdown
-    pub desp: Option<&'a str>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+#[derive(Builder, Serialize)]
+pub struct SendMsg<'a> {
+    #[builder(start_fn)]
+    #[serde(skip_serializing)]
+    client: &'a Client,
     /// 标签列表，多个标签使用竖线`|`分隔
-    pub tags: Option<&'a str>,
+    // 注意这个#[builder(filed)字段有顺序要求，需要放在start_fn之后，finish_fn之前
+    #[builder(field)]
+    #[serde(
+        serialize_with = "serialize_tags",
+        skip_serializing_if = "Vec::is_empty"
+    )]
+    tags: Vec<&'a str>,
+    /// 推送的标题
+    title: &'a str,
+    #[serde(rename = "desp", skip_serializing_if = "Option::is_none")]
+    /// 推送的正文内容，则为必填，支持markdown
+    description: Option<&'a str>,
     #[serde(skip_serializing_if = "Option::is_none")]
     /// 推送消息的简短描述，用于指定消息卡片的内容部分，尤其是在推送markdown的时候
-    pub short: Option<&'a str>,
+    short: Option<&'a str>,
+}
+
+fn serialize_tags<S>(tags: &[&str], serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    let tags_str = tags.join("|");
+    serializer.serialize_str(&tags_str)
+}
+
+impl<'a, S: send_msg_builder::State> SendMsgBuilder<'a, S> {
+    pub fn tag(mut self, tag: &'a str) -> Self {
+        self.tags.push(tag);
+        self
+    }
+
+    pub fn tags(mut self, tags: impl IntoIterator<Item = &'a str>) -> Self {
+        self.tags.extend(tags.into_iter());
+        self
+    }
 }
 
 /// 使用Server酱3
-pub struct ServerChan3 {
+pub struct Client {
     url: String,
-    client: reqwest::Client,
+    http_client: reqwest::Client,
 }
 
-impl ServerChan3 {
+#[bon]
+impl Client {
+    #[builder]
     pub fn new(uid: i32, key: &str) -> Self {
         Self {
             url: format!("https://{}.push.ft07.com/send/{}.send", uid, key),
-            client: reqwest::Client::new(),
+            http_client: reqwest::Client::new(),
         }
     }
 
-    pub async fn send_msg(&self, params: &SendParams<'_>) -> Result<(), Error> {
-        let resp = self
-            .client
-            .post(&self.url)
-            .json(&params)
-            .send()
-            .await
-            .map_err(|_| Error::AnyError("\"reqwest\" send error".to_string()))?;
+    pub fn send_msg(&self) -> SendMsgBuilder<'_> {
+        SendMsg::builder(self)
+    }
+}
 
-        if resp.status().is_success() {
-            Ok(())
-        } else {
-            Err(Error::AnyError(format!(
-                "Request failed with status: {}\n{}",
-                resp.status(),
-                resp.text()
-                    .await
-                    .unwrap_or("Failed to get response text".to_string())
-            )))
+impl SendMsg<'_> {
+    pub async fn send(&self) -> Result<(), Error> {
+        let client = self.client;
+        let resp = client
+            .http_client
+            .post(&client.url)
+            .json(self)
+            .send()
+            .await?;
+        if !resp.status().is_success() {
+            return Err(Error::API {
+                code: resp.status(),
+                body: resp.text().await.unwrap_or_default(),
+            });
         }
+
+        Ok(())
     }
 }
--- tests/server_chan/main.rs
+++ tests/server_chan/main.rs
@@ -1,64 +1,38 @@
 use serde::Deserialize;
-use toml::Table;
 use u_sdk::server_chan::*;
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
-    pub token: String,
+    pub uid: i32,
+    pub key: String,
 }
 
 impl Config {
     pub fn get_conf() -> Self {
         let file_str = std::fs::read_to_string("tests/server_chan/config.toml").unwrap();
         let conf = toml::from_str(&file_str).unwrap();
-
         conf
     }
 }
 
-fn get_server_chan3_conf() -> (i32, String) {
-    let file_str = std::fs::read_to_string("tests/server_chan/config.toml").unwrap();
-    let conf = toml::from_str::<Table>(&file_str).unwrap();
-    let uid = conf
-        .get("server_chan3_uid")
-        .unwrap()
-        .as_integer()
-        .expect("uid must be an integer") as i32;
-    let key = conf
-        .get("server_chan3_send_key")
-        .expect("server_chan3_send_key must be set")
-        // 这里需要as_str，如果不是，会返回`"xxx"`，包含引号
-        .as_str()
-        .expect("server_chan3_send_key must be a string");
-
-    (uid, key.to_owned())
-}
-
 #[tokio::test]
-async fn server_chan_turbo_test() {
+async fn server_chan_test() {
     let conf = Config::get_conf();
-    let server_chan = ServerChanTurbo::new(&conf.token);
-    let params = TurboSendParams {
-        title: "test--title",
-        description: Some("this is a description"),
-        is_no_ip: true,
-        ..Default::default()
-    };
-    server_chan.send_msg_turbo(params).await.unwrap();
-}
+    let client = Client::builder().uid(conf.uid).key(&conf.key).build();
 
-#[tokio::test]
-async fn server_chan3_test() {
-    let (uid, key) = get_server_chan3_conf();
-    let server_chan = ServerChan3::new(uid, &key);
-    let params = SendParams {
-        title: "test--title",
-        desp: Some("this is a description"),
-        short: Some("short"),
-        tags: Some("test"),
-    };
-    let res = server_chan.send_msg(&params).await;
-    if let Err(e) = res {
-        println!("error: {:?}", e);
+    let resp = client
+        .send_msg()
+        .title("test--title")
+        .description("this is a description")
+        .short("short")
+        .tag("123")
+        .tags(["tag1", "tag2"])
+        .build()
+        .send()
+        .await;
+    if let Err(e) = resp {
+        eprintln!("Error sending message: {}", e);
+    } else {
+        println!("Message sent successfully");
     }
 }
